3d92b202c6e482cb89504b2bd641680551b951ef
Rebeka Lewis Smith <bekabyx@gmail.com>
2023-12-08 19:03:17 +0000

    Part 2 done :)

--- day_1/advent_of_code_day_1.go
+++ day_1/advent_of_code_day_1.go
@@ -4,13 +4,15 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"regexp"
 	"strconv"
 	"unicode"
 )
 
 func main() {
 
-	fmt.Printf("Solution 1 answer:" + strconv.Itoa(solution_1()))
+	fmt.Println("Solution 1 answer:" + strconv.Itoa(solution_1()))
+	fmt.Println("Solution 2 answer:" + strconv.Itoa(solution_2()))
 }
 
 func solution_1() (total int) {
@@ -37,4 +39,66 @@ func solution_1() (total int) {
 	}
 
 	return total
+}
+
+func solution_2() (total int) {
+	file, _ := os.Open("input_1_1.txt")
+	bscanner := bufio.NewScanner(file)
+	number_regex, _ := regexp.Compile(`(\d|one|two|three|four|five|six|seven|eight|nine)`)
+
+	for bscanner.Scan() {
+		line := bscanner.Text()
+		var matches []string
+
+		for i := 0; i < len(line)-1; i++ {
+			submatches := (number_regex.FindAllString(line[i:], -1))
+			// had a fun time learning about variadic functions :)
+			matches = append(matches, submatches...)
+		}
+
+		length := len(matches)
+		first_number, _ := strconv.Atoi(matches[0])
+		last_number, _ := strconv.Atoi(matches[length-1])
+
+		if first_number == 0 {
+			first_number = getDigitFromString(matches[0])
+		}
+
+		if last_number == 0 {
+			if matches[0] == matches[length-1] {
+				last_number = first_number
+			} else {
+				last_number = getDigitFromString(matches[length-1])
+			}
+		}
+
+		total += (first_number * 10) + last_number
+	}
+
+	return total
+}
+
+func getDigitFromString(number_string string) (digit int) {
+	switch digit := number_string; digit {
+	case "one":
+		return 1
+	case "two":
+		return 2
+	case "three":
+		return 3
+	case "four":
+		return 4
+	case "five":
+		return 5
+	case "six":
+		return 6
+	case "seven":
+		return 7
+	case "eight":
+		return 8
+	case "nine":
+		return 9
+	default:
+		return 0
+	}
 }
